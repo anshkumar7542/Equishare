9a2f652188cc5d78af25b0eccfa359d14338700b
Ansh kumar <anshsingh7542037689@gmail.com>
2026-07-07 22:25:08 +0530

    final version

--- client/src/features/auth/SignupPage.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { AuthForm } from "./AuthForm";
-import { useAuth } from "./AuthProvider";
-
-export function SignupPage() {
-  const { user } = useAuth();
-  if (user) return <Navigate to="/dashboard" replace />;
-  return <AuthForm mode="signup" />;
-}
